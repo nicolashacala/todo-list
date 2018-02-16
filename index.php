554fdc84d9ddf100093ff633a82f634d81944d9b
nicolashacala <nicolas.hacala@gmail.com>
2018-02-16 16:01:56 +0100

    heroku ready

--- index.php
+++ index.php
@@ -10,26 +10,22 @@ require('contenu.php');
 </head>
 <body>
 
-<div id="list">
-      <div id="a-faire">
+<div class="tasks_display">
+    <div id="toDoDiv">
         <p class="h3">TO DO</p>
         <form>
-            
         </form>
     </div>
-    
-    <div id="done">
+    <div id="archiveDiv">
         <p class="h3">DONE</p>
-
     </div>
-  </div>
-
-	<div id="ajouter">
-        <p class="h2">AJOUTER UNE TACHE</p>
-           <form action="contenu.php" method="POST">
-                <input type="text" name="task" placeholder="La tâche à effectuer">
-                <input type="submit" name="submit" value="Ajouter" id="add">
-           </form>
+</div>
+<div class="add_tasks">
+    <p class="h2">Add a task</p>
+    <form action="contenu.php" method="POST">
+        <input type="text" name="task" placeholder="New Task...">
+        <input type="submit" value="Add">
+    </form>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
--- main.js
+++ main.js
@@ -6,16 +6,33 @@ xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200){
     var myObj = JSON.parse(this.responseText);
-    for (var i=0; i < myObj['todo'].length; i++) {
-      $("#a-faire").append("<label for='task"+i+"'><input type='checkbox' name='tache' value='false' id='task"+i+"' /><span>"+myObj['todo'][i]+"</span></label><br/>");
+    if(myObj['todo'] == null){
+      $('#toDoDiv').append("<p>Nothing to do...</p>")
+      if(myObj['archive'] != null){
+        for (var j=0; j < myObj['archive'].length; j++) {
+          $("#archiveDiv").append("<p>"+myObj['archive'][j]+"</p>");
+        }
+      }
     }
-    for (var j=0; j < myObj['archive'].length; j++) {
-      $("#done").append("<p>"+myObj['archive'][j]+"</p>");
+    else if(myObj['archive'] == null){
+      for (var i=0; i < myObj['todo'].length; i++) {
+        $("#toDoDiv").append("<label for='task"+i+"'><input type='checkbox' name='task' value='false' id='task"+i+"' /><span>"+myObj['todo'][i]+"</span></label><br/>");
+      }
+      $('#archiveDiv').append("<p>No tasks completed!</p>")
+    }
+    else{
+      for (var i=0; i < myObj['todo'].length; i++) {
+        $("#toDoDiv").append("<label for='task"+i+"'><input type='checkbox' name='task' value='false' id='task"+i+"' /><span>"+myObj['todo'][i]+"</span></label><br/>");
+      }
+      for (var j=0; j < myObj['archive'].length; j++) {
+        $("#archiveDiv").append("<p>"+myObj['archive'][j]+"</p>");
+      }
     }
     $("input[type=checkbox]").change(function() {
       if ($(this).is(':checked')) {
         $(this).attr('value', 'true');
         var selected = $("label[for='" +this.id +"']").text();
+        if(myObj['archive'] == null){myObj['archive'] = []}
         myObj['archive'].push(selected);
         myObj['todo'].splice($.inArray(selected, myObj['todo']), 1);
     
